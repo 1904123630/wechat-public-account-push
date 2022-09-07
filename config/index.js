6a404ea645dc7c9d18873f373119b1a503d2b33b
消失的橘子皮 <110590758+1904123630@users.noreply.github.com>
2022-09-07 18:15:52 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ export const config = {
   公众号APP_SECRET 
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-   APP_SECRET: "9e9a686d43e439502874b59f1518d3fb",
+  // APP_SECRET: "9e9a686d43e439502874b59f1518d3fb",
 
 
   USERS: [
