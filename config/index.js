9b2484c78e3745e6ad207b7bf91f56feaf09ab08
消失的橘子皮 <110590758+1904123630@users.noreply.github.com>
2022-09-07 22:56:04 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -76,7 +76,7 @@ export const config = {
    */
 
   // 回调消息模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "d9jj1t2DIomLGiWIXJfmJvnfBnws3w905-6KgAtRDio",
+  CALLBACK_TEMPLATE_ID: "Cyj1SHcQN4KJoiR7B8iQztUez5h_oYFRB3euw9f4ccc",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
