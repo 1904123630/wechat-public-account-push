ea3db08023ebc3fad6a43a58ace5e05450718ef5
消失的橘子皮 <110590758+1904123630@users.noreply.github.com>
2022-09-07 20:26:05 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -68,6 +68,14 @@ export const config = {
       city: "贵港",
       openUrl: "https://qm.qq.com/cgi-bin/qm/qr?k=7UHpH5pQ-X97dQcbAp-485DYj3oapChL&noverify=0"
     }, 
+     {
+      name: "你", 
+   //   id: "ouDkI6rZT8v1hGJ6eWSE8sPxeU8A",
+      useTemplateId: "ZHnY1sdooZxMfH7F3PbMY7Z1qF3vqJVk1Hj4jYo6KtU",
+      province: "广西",
+      city: "南宁",
+      openUrl: "https://qm.qq.com/cgi-bin/qm/qr?k=7UHpH5pQ-X97dQcbAp-485DYj3oapChL&noverify=0"
+    }, 
   ],
 
   /**
