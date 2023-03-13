e3356bfa108190410cf37b23a38e6f3133e6deae
消失的橘子皮 <110590758+1904123630@users.noreply.github.com>
2023-03-13 23:37:48 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -39,13 +39,13 @@ export const config = {
       // 所在省份
       province: "广西",
       // 所在城市
-      city: "贵港",
+      city: "南宁",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://qm.qq.com/cgi-bin/qm/qr?k=7UHpH5pQ-X97dQcbAp-485DYj3oapChL&noverify=0"
     }, 
       {
       // 想要发送的人的名字
-      name: "小笨蛋",
+      name: "钟玉欣",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "ouDkI6g8dMSKqMHQwcgoGp0sn54c",
       // 你想对他发送的模板消息的模板ID
@@ -53,7 +53,7 @@ export const config = {
       // 所在省份
       province: "广西",
       // 所在城市
-      city: "百色",
+      city: "南宁",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://qm.qq.com/cgi-bin/qm/qr?k=7UHpH5pQ-X97dQcbAp-485DYj3oapChL&noverify=0"
     }, 
