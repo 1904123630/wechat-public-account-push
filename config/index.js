09602487c01eecda53aaccc82d8c38f96085672f
消失的橘子皮 <110590758+1904123630@users.noreply.github.com>
2023-05-11 17:50:51 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -35,8 +35,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "ouDkI6hjGRp1N2C1uF365fw3nIBM",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "Uz7jZatW-WH-SdeRqOpU05gou78Mx2TTgbHGdWanf54
-",
+      useTemplateId: "Uz7jZatW-WH-SdeRqOpU05gou78Mx2TTgbHGdWanf54",
       // 所在省份
       province: "广西",
       // 所在城市
@@ -50,8 +49,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "ouDkI6g8dMSKqMHQwcgoGp0sn54c",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "Uz7jZatW-WH-SdeRqOpU05gou78Mx2TTgbHGdWanf54
-",
+      useTemplateId: "Uz7jZatW-WH-SdeRqOpU05gou78Mx2TTgbHGdWanf54",
       // 所在省份
      province: "广西",
       // 所在城市
